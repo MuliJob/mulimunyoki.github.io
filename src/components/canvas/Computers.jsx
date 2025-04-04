f16321f2e01d513ad6d15813cbabd2684d7270af
MuliJob <munyoki912@gmail.com>
2025-04-04 18:07:03 +0300

    updated model animation

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,15 +1,15 @@
-import React, { Suspense, useEffect, useState } from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { Suspense, useEffect, useState, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
-
+  
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
+      <hemisphereLight intensity={4} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -26,6 +26,29 @@ const Computers = ({ isMobile }) => {
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
+  );
+};
+
+// Fixed version that doesn't use the unused clock parameter
+const RotatingControls = ({ speed = 0.3 }) => {
+  const controlsRef = useRef();
+  
+  useFrame(() => {
+    if (controlsRef.current) {
+      // Auto-rotate the camera around the model
+      controlsRef.current.autoRotate = true;
+      controlsRef.current.autoRotateSpeed = speed;
+      controlsRef.current.update();
+    }
+  });
+
+  return (
+    <OrbitControls
+      ref={controlsRef}
+      enableZoom={false}
+      maxPolarAngle={Math.PI / 2}
+      minPolarAngle={Math.PI / 2}
+    />
   );
 };
 
@@ -55,18 +78,14 @@ const ComputersCanvas = () => {
 
   return (
     <Canvas
-      frameloop='demand'
+      frameloop='always'
       shadows
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
+        <RotatingControls speed={0.5} />
         <Computers isMobile={isMobile} />
       </Suspense>
 
