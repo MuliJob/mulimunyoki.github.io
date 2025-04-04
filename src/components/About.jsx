a488a677f20579fd637bba492a6ff3aabefa8bfb
MuliJob <munyoki912@gmail.com>
2025-04-04 13:01:32 +0300

    Ignoring lint warnings

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Tilt from "react-tilt";
+import { Tilt } from "react-tilt";
+// eslint-disable-next-line no-unused-vars
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -47,9 +48,9 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
+        I'm a skilled software developer with experience in Python and
+        JavaScript, and expertise in frameworks like Vue, Django, and
+        Flutter. I'm a quick learner and collaborate closely with clients to
         create efficient, scalable, and user-friendly solutions that solve
         real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
