317340e065bd37f20059cb7279a5aaaa2e179d54
MuliJob <munyoki912@gmail.com>
2025-04-05 19:44:16 +0300

    updated testimonial section

--- src/constants/index.js
+++ src/constants/index.js
@@ -144,27 +144,19 @@ const experiences = [
 const testimonials = [
   {
     testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
+      "I really admire everything you are doing. Thank you for your services.",
+    name: "Samwel Mathenge",
+    designation: "Founder",
+    company: "Digicraft",
+    image: "https://digicrafts.co.ke/static/resources/img/team/portfolio.jpeg",
   },
   {
     testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
-  },
-  {
-    testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
+      "Amazing services.",
+    name: "Dotty",
+    designation: "Owner",
+    company: "Softlife Luxury Spa",
+    image: "http://softlifespa.com/static/img/dotty.jpeg",
   },
 ];
 
