6e89f18e7f949d323a8e818c20f7ad105c50113d
MuliJob <munyoki912@gmail.com>
2025-04-05 11:39:00 +0300

    Added buttons for github, linkedin and cv

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,14 +12,14 @@ const Hero = () => {
       </div>
 
       <div
-        className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5 pointer-events-none`}
+        className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
-        <div className="flex flex-col justify-center items-center mt-5">
+        <div className="flex flex-col justify-center items-center mt-5 pointer-events-none">
           <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
 
-        <div>
+        <div className="pointer-events-auto">
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className="text-[#915EFF]">Munyoki</span>
           </h1>
@@ -28,6 +28,38 @@ const Hero = () => {
             <br className="sm:block hidden" />
             interfaces and web applications
           </p>
+          <div className="flex sm:flex-row mt-7">
+            <a
+                href="https://github.com/MuliJob"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="mb-4 sm:mb-0 sm:mr-4 green-pink-gradient p-[2px] rounded-[10px] shadow-card"
+            >
+                <button className="bg-[#151131] text-white px-4 py-2 rounded-lg transition duration-300 ease-in-out transform hover:scale-95">
+                    GitHub
+                </button>
+            </a>
+            <a
+                href="https://www.linkedin.com/in/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="mb-4 sm:mb-0 sm:mr-4 green-pink-gradient p-[2px] rounded-[10px] shadow-card"
+            >
+                <button className="bg-[#151131] text-white px-4 py-2 rounded-lg transition duration-300 ease-in-out transform hover:scale-95">
+                    LinkedIn
+                </button>
+            </a>
+            <a
+                href="https://drive.google.com/file/d/11NW478i0KJed4raslDAFq-WXJzSklexc/view?usp=sharing"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="mb-4 sm:mb-0 sm:mr-4 green-pink-gradient p-[2px] rounded-[10px] shadow-card cursor-pointer"
+            >
+                <button className="bg-[#151131] text-white px-4 py-2 rounded-lg transition duration-300 ease-in-out transform hover:scale-95">
+                    <strong>CV</strong>
+                </button>
+            </a>
+          </div>
         </div>
       </div>
 
